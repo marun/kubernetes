openshift/origin

UPSTREAM: 84568: test/e2e/apps: Skip or scale LB test per node count
Skip the "Deployment should not disrupt a cloud load-balancer's
connectivity during rollout" test if the number of nodes is less than 2;
otherwise, set the deployment's replicas equal to the lesser of 5 and the
number of nodes.

The test would fail if there were fewer nodes than replicas, but the test
needs at least 2 nodes, and the likelihood of failure absent the feature
under test increases with the number of replicas, so it is desirable to set
replicas to a higher value, within reason.

Follow-up to commit 980b6406b24b41bc0ef061624385f1f420708819.

* vendor/k8s.io/kubernetes/test/e2e/apps/deployment.go: Skip the
load-balancer connectivity test unless there are at least 2 nodes.
(testRollingUpdateDeploymentWithLocalTrafficLoadBalancer): Set replicas to
the min of 5 and the number of nodes.

vendor/k8s.io/kubernetes/test/e2e/apps/deployment.go
@@ -44,6 +44,7 @@ import (
 	"k8s.io/kubernetes/test/e2e/framework/replicaset"
 	e2eservice "k8s.io/kubernetes/test/e2e/framework/service"
 	testutil "k8s.io/kubernetes/test/utils"
+	"k8s.io/utils/integer"
 	utilpointer "k8s.io/utils/pointer"
 )
 
@@ -121,6 +122,7 @@ var _ = SIGDescribe("Deployment", func() {
 		testProportionalScalingDeployment(f)
 	})
 	ginkgo.It("should not disrupt a cloud load-balancer's connectivity during rollout", func() {
+		framework.SkipUnlessNodeCountIsAtLeast(2)
 		framework.SkipUnlessProviderIs("aws", "azure", "gce", "gke")
 		testRollingUpdateDeploymentWithLocalTrafficLoadBalancer(f)
 	})
@@ -869,7 +871,7 @@ func testRollingUpdateDeploymentWithLocalTrafficLoadBalancer(f *framework.Framew
 	name := "test-rolling-update-with-lb"
 	framework.Logf("Creating Deployment %q", name)
 	podLabels := map[string]string{"name": name}
-	replicas := int32(3)
+	replicas := int32(integer.IntMin(5, framework.TestContext.CloudConfig.NumNodes))
 	d := e2edeploy.NewDeployment(name, replicas, podLabels, AgnhostImageName, AgnhostImage, appsv1.RollingUpdateDeploymentStrategyType)
 	// NewDeployment assigned the same value to both d.Spec.Selector and
 	// d.Spec.Template.Labels, so mutating the one would mutate the other.

marun/kubernetes

Origin-commit: 3f09c3e5cd2f0a52e7549b376aefbb62b308dc7b

test/e2e/apps/deployment.go
@@ -50,6 +50,7 @@ import (
 	e2eservice "k8s.io/kubernetes/test/e2e/framework/service"
 	e2eskipper "k8s.io/kubernetes/test/e2e/framework/skipper"
 	testutil "k8s.io/kubernetes/test/utils"
+	"k8s.io/utils/integer"
 	utilpointer "k8s.io/utils/pointer"
 )
 
@@ -129,6 +130,7 @@ var _ = SIGDescribe("Deployment", func() {
 		testProportionalScalingDeployment(f)
 	})
 	ginkgo.It("should not disrupt a cloud load-balancer's connectivity during rollout", func() {
+		e2eskipper.SkipUnlessNodeCountIsAtLeast(2)
 		e2eskipper.SkipUnlessProviderIs("aws", "azure", "gce", "gke")
 		testRollingUpdateDeploymentWithLocalTrafficLoadBalancer(f)
 	})
@@ -870,7 +872,7 @@ func testRollingUpdateDeploymentWithLocalTrafficLoadBalancer(f *framework.Framew
 	name := "test-rolling-update-with-lb"
 	framework.Logf("Creating Deployment %q", name)
 	podLabels := map[string]string{"name": name}
-	replicas := int32(3)
+	replicas := int32(integer.IntMin(5, framework.TestContext.CloudConfig.NumNodes))
 	d := e2edeployment.NewDeployment(name, replicas, podLabels, AgnhostImageName, AgnhostImage, appsv1.RollingUpdateDeploymentStrategyType)
 	// NewDeployment assigned the same value to both d.Spec.Selector and
 	// d.Spec.Template.Labels, so mutating the one would mutate the other.